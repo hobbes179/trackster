4cc1adf1dd8ecd2a51ed0e75ea08a5950d165525
Frank Hoey <hobbes179@gmail.com>
2018-05-30 18:33:24 -0500

    added search event handler

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,8 +1,15 @@
+$(document).ready(function() {
+  $('#search-button').click(function(){
+    Trackster.searchTracks($('#search-input').val());
+    $('#search-input').val('');
+  });
+});
+
 var Trackster = {};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
-  Append each "row" to the container in the body to display all tracks. 
+  Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
 
@@ -12,6 +19,6 @@ Trackster.renderTracks = function(tracks) {
   Given a search term as a string, query the LastFM API.
   Render the tracks given in the API query response.
 */
-Trackster.searchTracksByTitle = function(title) {
-
+Trackster.searchTracks = function(searchTerm) {
+  console.log(`Searching for ${searchTerm}`);
 };
