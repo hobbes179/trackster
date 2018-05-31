a50786579bd716fc004706c71f42ed5f7164f933
Frank Hoey <hobbes179@gmail.com>
2018-05-30 20:17:23 -0500

    search results render in html

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,18 +1,39 @@
+/*
+Application name 	trackster
+API key 	f0faf492ddca3cb5e4693e805dda84de
+Shared secret 	ecc7656fc005cd9a2fa02d131570fa41
+Registered to 	hobbes179
+*/
+
 $(document).ready(function() {
   $('#search-button').click(function(){
     Trackster.searchTracks($('#search-input').val());
     $('#search-input').val('');
   });
+
 });
 
 var Trackster = {};
+const $API_KEY = 'f0faf492ddca3cb5e4693e805dda84de';
+
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
-Trackster.renderTracks = function(tracks) {
-
+Trackster.renderTracks = function(track) {
+  console.log(track.image[1]["#text"]);
+  $('#track-list').append(`
+    <div class='row track'>
+      <a id='play-button' class='col-xs-1 col-xs-offset-1' href=${track.url}>
+        <i class='fa fa-play-circle-o fa-2x'></i>
+      </a>
+      <div class='col-xs-3'>${track.name}</div>
+      <div class='col-xs-2'>${track.artist}</div>
+      <div class='col-xs-2'>${track.listeners}</div>
+      <img class='col-xs-2' src=${track.image[1]["#text"]}/>
+    </div>
+  `);
 };
 
 /*
@@ -21,4 +42,17 @@ Trackster.renderTracks = function(tracks) {
 */
 Trackster.searchTracks = function(searchTerm) {
   console.log(`Searching for ${searchTerm}`);
+  $.ajax({
+    url: `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${searchTerm}&api_key=${$API_KEY}&format=json`,
+    dataType: 'json',
+    success: function(response) {
+      for (i=0; i<response.results.trackmatches.track.length; i++){
+        Trackster.renderTracks(response.results.trackmatches.track[i]);
+      };
+    },
+    error: function(response) {
+      console.log("ERROR:");
+      console.log(response);
+    }
+  });
 };
